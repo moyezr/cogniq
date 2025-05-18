4ddb78d90d8d169bf7763a6a82f2a3f47cdf8cdb
Moyez Rabbani <moyezrabbani@gmail.com>
2025-05-18 20:49:25 +0530

    fixed new page

--- app/(dashboard)/play/new/page.tsx
+++ app/(dashboard)/play/new/page.tsx
@@ -1,5 +1,4 @@
 import QuizCreationForm from "@/components/quiz-creation-form";
-import { Skeleton } from "@/components/ui/skeleton";
 import React from "react";
 
 type Props = {};
@@ -9,29 +8,13 @@ export const metadata = {
 };
 const QuizPage = async (props: Props) => {
   return (
+
     <main className="pt-8 flex flex-col gap-8 justify-center items-center min-h-[60vh]">
-      <div className="w-full sm:w-[70%] md:w-[40%] lg:w-[40%]">
-        <div className="bg-slate-900 p-6 rounded-lg shadow-lg border border-slate-800">
-          {/* Form Title Skeleton */}
-          <Skeleton className="w-48 h-8 mx-auto mb-6" />
-
-          {/* Topic Field Skeleton */}
-          <div className="mb-6">
-            <Skeleton className="w-24 h-5 mb-2" />
-            <Skeleton className="w-full h-10 rounded-md" />
-          </div>
-
-
-          {/* Number of Questions Skeleton */}
-          <div className="mb-6">
-            <Skeleton className="w-48 h-5 mb-2" />
-            <Skeleton className="w-full h-10 rounded-md" />
-          </div>
-
-
-          {/* Submit Button Skeleton */}
-          <Skeleton className="w-full h-12 rounded-md mt-6" />
-        </div>
+      {/* <h1 className='tracking-tighter text-7xl font-bold'>
+            Choose Your Topic
+          </h1> */}
+      <div className="w-full sm:w-[70%] md:w-[60%] lg:w-[50%]">
+        <QuizCreationForm />
       </div>
     </main>
   );
